c18436c804b26eeab73e24d4e3ba0923a6eca0fb
TheChok <97156127+TheChok@users.noreply.github.com>
2022-02-03 21:25:58 +0900

    Update CondStar03.java

--- src/conTest/CondStar03.java
+++ src/conTest/CondStar03.java
@@ -11,14 +11,14 @@ class StarExam06 {
 		int lineNum = 5;
 		for(int line = 0; line < lineNum; line++) {
 			for(int col = 0; col < lineNum; col++) {
-				if(line <= lineNum / 2) {						// 윗부분
-					if(line+col <= lineNum / 2 - 1) 			System.out.print(".");
-					else if(col-line >= lineNum / 2 + 1)		System.out.print("_");
-					else 										System.out.print("★");
-				} else if(line > lineNum / 2) {					// 아랫부분
-					if(line-col >= lineNum / 2 + 1)				System.out.print(".");
-					else if(line+col >= lineNum / 2 * 3 + 1)	System.out.print("_");
-					else										System.out.print("★");
+				if(line <= lineNum / 2) {			 // 윗부분
+					if(line+col <= lineNum / 2 - 1) 	 System.out.print(".");
+					else if(col-line >= lineNum / 2 + 1)	 System.out.print("_");
+					else 					 System.out.print("★");
+				} else if(line > lineNum / 2) {			 // 아랫부분
+					if(line-col >= lineNum / 2 + 1)		 System.out.print(".");
+					else if(line+col >= lineNum / 2 * 3 + 1) System.out.print("_");
+					else					 System.out.print("★");
 				}
 			} System.out.println();
 		} // for(int line)
@@ -30,12 +30,12 @@ class StarExam07 {
 		int lineNum = 5;
 		for(int line = 0; line < lineNum; line++) {
 			for(int col = 0; col < lineNum; col++) {
-				if(line <= lineNum/2) {												// 윗부분
-					if((line+col <= lineNum/2-1) ||(col-line >= lineNum/2+1))		System.out.print("_");
-					else															System.out.print("★");
-				} else if(line > lineNum/2) {										// 아랫부분
-					if((line-col >= lineNum/2+1) || (line+col >= lineNum/2*3+1))	System.out.print("_"); 
-					else															System.out.print("★");
+				if(line <= lineNum/2) {						     // 윗부분
+					if((line+col <= lineNum/2-1) ||(col-line >= lineNum/2+1))    System.out.print("_");
+					else							     System.out.print("★");
+				} else if(line > lineNum/2) {					     // 아랫부분
+					if((line-col >= lineNum/2+1) || (line+col >= lineNum/2*3+1)) System.out.print("_"); 
+					else							     System.out.print("★");
 				}
 			} System.out.println();						
 		} // for(int line)
