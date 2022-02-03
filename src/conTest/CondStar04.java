7c2979e79c37de43f97f2b123d342f7b4ace5fd3
TheChok <97156127+TheChok@users.noreply.github.com>
2022-02-03 21:24:43 +0900

    Update CondStar04.java

--- src/conTest/CondStar04.java
+++ src/conTest/CondStar04.java
@@ -10,14 +10,14 @@ public class CondStar04 {
 class StarExam08 {
 	StarExam08() {
 		int lineNumber = 5;
-		for(int line = lineNumber; line >= 1; line--) {			// 역삼각형부분
-			for(int col = 0; col < lineNumber-line; col++)	{	System.out.print("_");	}	// 빈공간을 만들고
-			for(int k = 1; k < line * 2; k++) 				{	System.out.print("★");	}	// ★을 이어 붙인다.
+		for(int line = lineNumber; line >= 1; line--) {		// 역삼각형부분
+			for(int col = 0; col < lineNumber-line; col++)	{System.out.print("_");}	 // 빈공간을 만들고
+			for(int k = 1; k < line * 2; k++) 		{System.out.print("★");}	// ★을 이어 붙인다.
 			System.out.println();
 		}
-		for(int line = 1; line <= lineNumber; line++) {			// 정삼각형부분
-			for(int col = 1; col <= lineNumber-line; col++)	{	System.out.print("_");	}	// 빈공간을 만들고
-			for(int col = 1; col <= line * 2 - 1; col++) 	{	System.out.print("★");	}	// ★을 이어 붙인다. 
+		for(int line = 1; line <= lineNumber; line++) {		// 정삼각형부분
+			for(int col = 1; col <= lineNumber-line; col++)	{System.out.print("_");}	 // 빈공간을 만들고
+			for(int col = 1; col <= line * 2 - 1; col++) 	{System.out.print("★");}	// ★을 이어 붙인다. 
 			System.out.println();
       	}
 	} // End - StarExam08()
@@ -28,9 +28,9 @@ class StarExam09 {
 		int lineNumber = 5;
 		for(int row = 0; row < lineNumber; row++) {
 			for(int col = 0; col < lineNumber; col++) {
-				if(row == col)						System.out.print("★");	// 대각선1
+				if(row == col)				System.out.print("★");	// 대각선1
 				else if(row+col == lineNumber-1)	System.out.print("★");	// 대각선2
-				else								System.out.print(" ");	// 빈공간
+				else					System.out.print(" ");	 // 빈공간
 			} System.out.println();
 		}
 	} // End - StarExam09()
@@ -42,7 +42,7 @@ class StarExam10 {
 		for(int row = 0; row < lineNumber; row++) {
 			for(int col = 0; col < lineNumber; col++) {
 				if((row == col) || (row+col == lineNumber-1))	System.out.print("★");	// 대각선1, 대각선2
-				else											System.out.print(" ");	// 나머지 빈공간.
+				else						System.out.print(" ");	// 나머지 빈공간.
 			} System.out.println();
 		}
 	} // End - StarExam10()
