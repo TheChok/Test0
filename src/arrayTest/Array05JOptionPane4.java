a4741fdd1f75fa125fa653ed0014b79c1894a54f
TheChok <97156127+TheChok@users.noreply.github.com>
2022-02-03 21:30:38 +0900

    Update Array05JOptionPane4.java

--- src/arrayTest/Array05JOptionPane4.java
+++ src/arrayTest/Array05JOptionPane4.java
@@ -11,7 +11,7 @@ class testJOptionPane4 {
 	testJOptionPane4() {
 		// 양의 정수 10개를 배열에 저장하고, 그 중에서 3의 배수만 출력
 		int[] 	arr4 = new int[10];
-		int		num = 0;
+		int	num = 0;
 		
 		System.out.println("양의 정수 10개를 입력하세요");
 		for(int i = 0; i < arr4.length; i++) {
@@ -24,7 +24,7 @@ class testJOptionPane4 {
 		for(int i = 0; i < arr4.length-1; i++) {
 			for(int j = 0; j < arr4.length-1-i; j++) {
 				if(arr4[j] > arr4[j+1]) { //왼쪽값이 크면 값을 오른쪽으로 밀어낸다.
-					int imsi = arr4[j];		arr4[j] = arr4[j+1];	arr4[j+1] = imsi;
+					int imsi = arr4[j];	 arr4[j] = arr4[j+1];	  arr4[j+1] = imsi;
 				}
 			}
 		}
@@ -32,7 +32,7 @@ class testJOptionPane4 {
 		System.out.println("입력된 값");
 		for(int i :arr4) {
 			if(i == arr4.length-1)	System.out.print(arr4[i]);
-			else 					System.out.print(arr4[i] + ", ");
+			else 			System.out.print(arr4[i] + ", ");
 		}	System.out.println();
 		
 		System.out.println("3의 배수는?");
