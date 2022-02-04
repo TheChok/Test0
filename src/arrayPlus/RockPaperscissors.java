cb36815dcca23f4a45edf0f177ad89a41d653202
TheChok <97156127+TheChok@users.noreply.github.com>
2022-02-04 16:52:39 +0900

    Update RockPaperscissors.java

--- src/arrayPlus/RockPaperscissors.java
+++ src/arrayPlus/RockPaperscissors.java
@@ -6,8 +6,8 @@ public class RockPaperscissors {
 	public static void main(String[] args) {
 		// 컴퓨터와 하는 가위바위보 게임
 		Scanner sc = new Scanner(System.in);
-		String	 player, computer;					boolean exitFlag = true;		
-		String[] sArr = {"가위", "바위", "보"};		int[] 	 score = new int[3];
+		String	 player, computer;		    boolean exitFlag = true;		
+		String[] sArr = {"가위", "바위", "보"};	int[] score = new int[3];
 		
 		// 게임 시작 - 사용자 선택 => 컴퓨터 선택 => 판정 => 반복(종료).
 		while(exitFlag) {
@@ -23,23 +23,23 @@ public class RockPaperscissors {
 			System.out.println("컴퓨터 : " + computer);
 
 			// 판정 - 입력된 값은 가위, 바위, 보, 종료 4가지.
-			if(player.equals("가위")) {			 // 플레이어 "가위"
-				if(computer.equals("가위")) 	 {		System.out.println("무승부");		score[1]++;		}	// score[1] 무승부
-				else if(computer.equals("바위")) {		System.out.println("컴퓨터 승");	score[2]++;		}	// score[2] 컴퓨터 승
-				else if(computer.equals("보")) 	 {		System.out.println("플레이어 승");	score[0]++;		}	// score[0] 플레이어 승
-			} else if(player.equals("바위")) {   // 플레이어 "바위"
-				if(computer.equals("가위")) 	 {		System.out.println("플레이어 승");	score[0]++;		}
-				else if(computer.equals("바위")) {		System.out.println("무승부");		score[1]++;		}
-				else if(computer.equals("보")) 	 {		System.out.println("컴퓨터 승");	score[2]++;		}				
-			} else if(player.equals("보")) {	 // 플레이어 "보"	
-				if(computer.equals("가위")) 	 {		System.out.println("컴퓨터 승");	score[2]++;		}
-				else if(computer.equals("바위")) {		System.out.println("플레이어 승");	score[0]++;		}
-				else if(computer.equals("보")) 	 {		System.out.println("무승부");		score[1]++;		}				
+			if(player.equals("가위")) {		// 플레이어 "가위"
+				if(computer.equals("가위"))      { System.out.println("무승부");	 score[1]++; } // score[1] 무승부
+				else if(computer.equals("바위")) { System.out.println("컴퓨터 승");	score[2]++; } // score[2] 컴퓨터 승
+				else if(computer.equals("보")) 	{ System.out.println("플레이어 승");	score[0]++; } // score[0] 플레이어 승
+			} else if(player.equals("바위")) {   	// 플레이어 "바위"
+				if(computer.equals("가위"))      { System.out.println("플레이어 승"); score[0]++; }
+				else if(computer.equals("바위")) { System.out.println("무승부");	 score[1]++; }
+				else if(computer.equals("보")) 	{System.out.println("컴퓨터 승");	 score[2]++; }				
+			} else if(player.equals("보")) {	 	// 플레이어 "보"	
+				if(computer.equals("가위"))      { System.out.println("컴퓨터 승");   score[2]++; }
+				else if(computer.equals("바위")) { System.out.println("플레이어 승"); score[0]++; }
+				else if(computer.equals("보")) 	{ System.out.println("무승부");	  score[1]++; }				
 			} else if(player.equals("종료")) {
-				exitFlag = false; 	   continue; // "종료" - exitFlag를 false로 바꾸고 반복문을 벗어난다.			
+				exitFlag = false;		   continue; // "종료" - exitFlag를 false로 바꾸고 반복문을 벗어난다.			
 			} else {
 				System.out.println("가위, 바위, 보 중 하나를 입력하셔야 합니다.");
-				System.out.println("종료를 입력하면 프로그램이 끝납니다.");	// 오류확인용
+				System.out.println("종료를 입력하면 프로그램이 끝납니다."); // 오류확인용
 			}
 			// 게임 현황을 보여준다.
 			System.out.println("현재 사용자의 전적은  " + score[0]+"승 " + score[1]+"무 " + score[2]+"패  입니다.");
